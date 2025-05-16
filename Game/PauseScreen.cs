c2ca43331da8126537dc33c92df69f41a1771464
JosVroegindeweij <jos.vroegindeweij@hotmail.com>
2025-05-17 00:04:26 +0200

    ✨ Introduce pausing and unpausing functionality

--- Game/PauseScreen.cs
+++ Game/PauseScreen.cs
@@ -6,16 +6,28 @@ public partial class PauseScreen : RichTextLabel
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("PauseScreen is ready");
+		GetTree().Paused = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var keyPressed = Input.IsAnythingPressed();
-		if (keyPressed)
+		GD.Print("Pause pressed: " + Input.IsActionPressed("pause"));
+		if (Input.IsActionPressed("pause"))
+		{
+			// Pause the game
+			GD.Print("Pausing the game");
+			GetTree().SetDeferred("paused", true);
+			Show();
+		}
+
+		if (GetTree().Paused && Input.IsActionPressed("unpause"))
 		{
 			// Unpause the game
-			GetTree().Paused = false;
+			GD.Print("Unpausing the game");
+			GetTree().SetDeferred("paused", false);
+			Hide();
 		}
 	}
 }
--- Game/Player.cs
+++ Game/Player.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	private const double TIME_BEFORE_START_FALLING = 3;
+	private const double TIME_BEFORE_START_FALLING = 1.5;
 	private const float GRAVITY = 9.8f;
 	private const double JUMP_VERTICAL_ACCELERATION = -2.5;
 
@@ -14,18 +14,12 @@ public partial class Player : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Player is ready");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var escPressed = Input.IsActionPressed("ui_cancel");
-		if (escPressed)
-		{
-			// Pause the game
-			GetTree().Paused = true;
-			return;
-		}
 		_timeSinceStart += delta;
 
 		if (_timeSinceStart <= TIME_BEFORE_START_FALLING)
@@ -34,28 +28,20 @@ public partial class Player : CharacterBody2D
 			return;
 		}
 
-		var acceptPressed = Input.IsActionPressed("ui_accept");
-		var upPressed = Input.IsActionPressed("ui_up");
-		var downPressed = Input.IsActionPressed("ui_down");
-		var leftPressed = Input.IsActionPressed("ui_left");
-		var rightPressed = Input.IsActionPressed("ui_right");
-
-		var jumped = acceptPressed || upPressed || downPressed || leftPressed || rightPressed;
-
-		if (jumped)
+		if (Input.IsActionPressed("jump"))
 		{
-			Jump(delta);
+			Jump();
 		}
 
-		Fall(delta);
+		Fall();
 	}
 
-	private void Jump(double delta)
+	private void Jump()
 	{
 		_verticalAcceleration = JUMP_VERTICAL_ACCELERATION;
 	}
 
-	private void Fall(double delta)
+	private void Fall()
 	{
 		var fallingAcceleration = 0.003f * GRAVITY;
 		_verticalAcceleration += fallingAcceleration;
