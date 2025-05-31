b12b50c1120f2c84fd8990d56f042a5b4048391c
JosVroegindeweij <jos.vroegindeweij@hotmail.com>
2025-06-01 01:17:12 +0200

    ✨ Add bottom screen edge

--- Game/Game.cs
+++ Game/Game.cs
@@ -13,11 +13,11 @@ public partial class Game : Node2D
 				angle: 0,
 				position: new Vector2(screenSize.X / 2f, 0)
 			);
-		// GetNode<ScreenEdge>("BottomScreenEdge")
-		// 	.Setup(
-		// 		size: new Vector2(x: screenSize.X, y: screenEdgeThickness),
-		// 		angle: 0,
-		// 		position: new Vector2(screenSize.X / 2f, screenSize.Y)
-		// 	);
+		GetNode<ScreenEdge>("BottomScreenEdge")
+			.Setup(
+				size: new Vector2(x: screenSize.X, y: screenEdgeThickness),
+				angle: 0,
+				position: new Vector2(screenSize.X / 2f, screenSize.Y)
+			);
 	}
 }
